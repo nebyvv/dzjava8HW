bdf5f800abc12d08969b8fd6f5cfe3d5d4d8c0a2
Ruslan <149687540+nebyvv@users.noreply.github.com>
2024-12-08 04:45:34 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//////////////ZADANIYA1//////////
+// const links = document.querySelectorAll("a");
+
+// links.forEach((link) => {
+//   const href = link.getAttribute("href");
+//   if (href.startsWith("http://") || href.startsWith("https://")) {
+//     link.classList.add("external-link");
+//   }
+// });
+/////////////////////////////////////////////////////
+//////////////////////
+//////////////////////TASK3
+// const bookList = document.getElementById("book-list");
+
+// bookList.addEventListener("click", (event) => {
+//   if (event.target.tagName !== "LI") return;
+
+//   event.target.classList.toggle("selected");
+// });
